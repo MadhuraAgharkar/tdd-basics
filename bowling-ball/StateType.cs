b29419d3478547010502f63c5bbfc51008fcc6f5
MadhuraAgharkar <70260114+MadhuraAgharkar@users.noreply.github.com>
2020-09-04 11:40:14 +0530

    TDD1 : All passed test cases

---
+++ bowling-ball/StateType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingBall
+{
+    public enum StateType
+    {
+        Spare,
+        Strike,
+        Invalid,
+        None
+    }
+}
